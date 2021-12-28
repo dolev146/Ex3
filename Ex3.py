17f6fd0cc3de5eff0e15df8b54be3aecd93d821e
Dolev Dublon <62290677+dolev146@users.noreply.github.com>
2021-12-28 12:23:22 +0200

    example of how to test our code added

--- Ex3.py
+++ Ex3.py
@@ -1,5 +1,6 @@
 import sys
 
+from Classes.GraphAlgo import GraphAlgo
 from gui import constants
 from gui.newcontrol import GUI
 
@@ -13,4 +14,10 @@ if __name__ == '__main__':
         print("no json path added ")
         print("exit program")
         sys.exit()
-
+    # if u mant tot test outputs on terminal just comment the above lines, to stop the gui
+    # and use the example bellow
+    # ga = GraphAlgo()
+    # ga.load_from_json("./data/A0.json")
+    # print(ga.centerPoint())
+    # print(ga.TSP([7, 5, 3]))
+    # print(ga.shortest_path(1, 5))
